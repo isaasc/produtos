47e51eaf44b49ed8123662d30f6807be545f541c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-19 21:19:10 -0300

    first commit

--- src/main/java/br/fiap/app/exemplo/models/Produto.java
+++ src/main/java/br/fiap/app/exemplo/models/Produto.java
@@ -4,40 +4,54 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-@Entity //cria entidade
+@Entity // cria entidade
 public class Produto {
-	
-	@Id //primary key
-	@GeneratedValue(strategy=GenerationType.AUTO) //auto increment
-	private Long id; 
+
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.AUTO) // auto increment
+	private Long id;
+
+	@NotBlank(message = "Produto precisa ter nome.")
 	private String nome;
-	private Long idCategoria; 
-	private Long idFornecedor; 
-	
-	
+	private Long idCategoria;
+
+	@NotNull(message = "O fornecedor é obrigatório.")
+	@Min(1)
+	private Long idFornecedor;
+
 	public Long getIdCategoria() {
 		return idCategoria;
 	}
+
 	public void setIdCategoria(Long idCategoria) {
 		this.idCategoria = idCategoria;
 	}
+
 	public Long getIdFornecedor() {
 		return idFornecedor;
 	}
+
 	public void setIdFornecedor(Long idFornecedor) {
 		this.idFornecedor = idFornecedor;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
-	} 
+	}
 }
